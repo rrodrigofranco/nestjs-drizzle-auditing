43ce23db135ac241b6f3fd1c62088c8ec6f08c0f
Rodrigo Franco <francorodrigognr@yahoo.com.br>
2025-06-05 22:20:39 -0300

    first commit

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,5 @@ export * from './audit.module';
 export * from './audit.decorator';
 export * from './audit.service';
 export * from './audit.interceptor';
-
-// src/interfaces/audit-options.interface.ts
-export interface AuditOptions {
-  entity: string;
-  action?: 'create' | 'update' | 'delete';
-  ignoreFields?: string[];
-}
+export * from './interfaces/audit-options.interface';
+export * from './interfaces/audit-module-options.interface'; // <-- adicione isso

73ed5c36cb0ea873e1b016f7d744311b09e2c524
Rodrigo Franco <francorodrigognr@yahoo.com.br>
2025-06-05 22:18:31 -0300

    first commit

--- src/audit.module.ts
+++ src/audit.module.ts
@@ -1,10 +1,23 @@
-import { Module, Global } from '@nestjs/common';
+import { Module, Global, DynamicModule } from '@nestjs/common';
 import { AuditInterceptor } from './audit.interceptor';
 import { AuditService } from './audit.service';
+import { AuditModuleOptions } from './interfaces/audit-module-options.interface';
 
 @Global()
-@Module({
-  providers: [AuditService, AuditInterceptor],
-  exports: [AuditService, AuditInterceptor],
-})
-export class AuditModule {}
+@Module({})
+export class AuditModule {
+  static forRootAsync(options: AuditModuleOptions): DynamicModule {
+    return {
+      module: AuditModule,
+      providers: [
+        {
+          provide: 'AUDIT_SCHEMA',
+          useValue: options.schema,
+        },
+        AuditService,
+        AuditInterceptor,
+      ],
+      exports: [AuditService, AuditInterceptor],
+    };
+  }
+}

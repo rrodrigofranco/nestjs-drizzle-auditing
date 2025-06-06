89b0f66630da54a4f7b5f11fe9d985d8632292eb
Rodrigo Franco <francorodrigognr@yahoo.com.br>
2025-06-06 15:02:34 -0300

    second commit

--- src/audit.module.ts
+++ src/audit.module.ts
@@ -2,32 +2,40 @@ import { Module, DynamicModule, Global } from '@nestjs/common';
 import { AuditService } from './audit.service';
 import { AuditInterceptor } from './audit.interceptor';
 
+export interface AuditModuleAsyncOptions {
+  imports?: any[];
+  inject?: any[];
+  useFactory: (...args: any[]) => Promise<{
+    schema: any;
+    drizzleClient: any;
+  }> | {
+    schema: any;
+    drizzleClient: any;
+  };
+}
+
 @Global()
 @Module({})
 export class AuditModule {
-  static forRootAsync(options: {
-      imports?: any[];
-      inject?: any[];
-      useFactory: (...args: any[]) => Promise<{ schema: any; drizzleClient: any }> | { schema: any; drizzleClient: any };
-    }): DynamicModule {
-      return {
-        module: AuditModule,
-        imports: options.imports || [],
-        providers: [
-          {
-            provide: 'AUDIT_SCHEMA',
-            useFactory: async (...args: any[]) => (await options.useFactory(...args)).schema,
-            inject: options.inject || [],
-          },
-          {
-            provide: 'DRIZZLE_CLIENT',
-            useFactory: async (...args: any[]) => (await options.useFactory(...args)).drizzleClient,
-            inject: options.inject || [],
-          },
-          AuditService,
-          AuditInterceptor,
-        ],
-        exports: [AuditService, AuditInterceptor],
-      };
-    }
+  static forRootAsync(options: AuditModuleAsyncOptions): DynamicModule {
+    return {
+      module: AuditModule,
+      imports: options.imports || [],
+      providers: [
+        {
+          provide: 'AUDIT_SCHEMA',
+          useFactory: async (...args: any[]) => (await options.useFactory(...args)).schema,
+          inject: options.inject || [],
+        },
+        {
+          provide: 'DRIZZLE_CLIENT',
+          useFactory: async (...args: any[]) => (await options.useFactory(...args)).drizzleClient,
+          inject: options.inject || [],
+        },
+        AuditService,
+        AuditInterceptor,
+      ],
+      exports: [AuditService, AuditInterceptor],
+    };
+  }
 }
